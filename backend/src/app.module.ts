446c1e0cb3e6d7206987a26569d51b6b44503161
미플 <devmeeple@gmail.com>
2023-12-16 11:33:02 +0900

    [NMNJ-3] 데이터베이스 설정하기
    - TypeORM 연결
    - Config 패키지 추가

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,36 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import * as process from 'process';
+import {
+  ENV_DB_DATABASE_KEY,
+  ENV_DB_HOST_KEY,
+  ENV_DB_PASSWORD_KEY,
+  ENV_DB_PORT_KEY,
+  ENV_DB_USERNAME_KEY,
+} from './config/env-keys.const';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env', // 환경변수 경로
+      isGlobal: true, // 전역에서 모듈을 사용할 수 있도록 함
+      cache: true, // 캐시를 사용해서 성능(읽기 속도)을 향상
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env[ENV_DB_HOST_KEY],
+      port: parseInt(process.env[ENV_DB_PORT_KEY]),
+      username: process.env[ENV_DB_USERNAME_KEY],
+      password: process.env[ENV_DB_PASSWORD_KEY],
+      database: process.env[ENV_DB_DATABASE_KEY],
+      entities: [],
+      synchronize: true, // 동기화 개발 환경에서만 활성화 할 것
+      logging: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
