8d7de76c2a313897b14944eece7dbd46c0bab978
미플 <devmeeple@gmail.com>
2023-12-16 11:33:02 +0900

    [NMNJ-3] 데이터베이스 설정하기
    - TypeORM 연결
    - Config 패키지 추가

---
+++ backend/src/config/env-keys.const.ts
@@ -0,0 +1,14 @@
+/**
+ * env 파일 선언이 필요하다
+ * HOST: 호스트
+ * PORT: 포트
+ * USERNAME: 사용자 이름
+ * PASSWORD: 사용자 비밀번호
+ * DATABASE: 데이터베이스 이름
+ */
+
+export const ENV_DB_HOST_KEY = 'DB_HOST';
+export const ENV_DB_PORT_KEY = 'DB_PORT';
+export const ENV_DB_USERNAME_KEY = 'DB_USERNAME';
+export const ENV_DB_PASSWORD_KEY = 'DB_PASSWORD';
+export const ENV_DB_DATABASE_KEY = 'DB_DATABASE';
