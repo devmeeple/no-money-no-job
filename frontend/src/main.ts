bc9db5cf4ca290f334820417a881eb7b35dec94e
미플 <devmeeple@gmail.com>
2024-01-20 21:07:34 +0900

    feat: 화면 디자인
    - sass 추가, 프리텐다드 폰트 추가, normalize.css 추가

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,18 +1,20 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
 
-import App from './App.vue'
-import router from './router'
+import App from './App.vue';
+import router from './router';
 
-import 'bootstrap/dist/css/bootstrap-utilities.css'
+import 'normalize.css';
 
-const app = createApp(App)
+import 'bootstrap/dist/css/bootstrap-utilities.css';
 
-app.use(ElementPlus)
-app.use(createPinia())
-app.use(router)
+const app = createApp(App);
+
+app.use(ElementPlus);
+app.use(createPinia());
+app.use(router);
 
 // mount 메서드 호출로 렌더링 시작 => nest bootstrap
-app.mount('#app')
+app.mount('#app');
--- frontend/src/views/WriteView.vue
+++ frontend/src/views/WriteView.vue
@@ -1,17 +1,3 @@
-<template>
-  <div>
-    <el-input v-model="title" placeholder="제목을 입력해주세요" />
-  </div>
-
-  <div class="mt-2">
-    <el-input v-model="content" type="textarea" rows="15" />
-  </div>
-
-  <div class="mt-2">
-    <el-button type="primary" @click="write()">글 작성완료</el-button>
-  </div>
-</template>
-
 <script setup lang="ts">
 import { ref } from 'vue';
 import axios from 'axios';
@@ -35,5 +21,21 @@ const write = function () {
     });
 };
 </script>
+
+<template>
+  <div>
+    <el-input v-model="title" placeholder="제목을 입력해주세요" />
+  </div>
+
+  <div class="mt-2">
+    <el-input v-model="content" type="textarea" rows="15" />
+  </div>
+
+  <div class="mt-2">
+    <div class="d-flex justify-content-end">
+      <el-button type="primary" @click="write()">작성완료</el-button>
+    </div>
+  </div>
+</template>
 
 <style scoped></style>
